a3f3092bec190a7d2d24e4e9203afb86a429c1c0
philippguggi <58741046+philippguggi@users.noreply.github.com>
2020-04-20 08:28:50 +0200

    First commit

---
+++ StudentDataBase/src/com/PhilippGuggi/StudentDataBase/Student.java
@@ -0,0 +1,5 @@
+package com.PhilippGuggi.StudentDataBase;
+
+public class Student {
+
+}
---
+++ StudentDataBase/src/com/PhilippGuggi/StudentDataBase/StudentDataBaseApp.java
@@ -0,0 +1,7 @@
+package com.PhilippGuggi.StudentDataBase;
+
+public class StudentDataBaseApp {
+	public static void main(String[] args) {
+		
+	}
+}
