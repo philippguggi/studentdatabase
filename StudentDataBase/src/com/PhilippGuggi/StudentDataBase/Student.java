043f6e3ed08836d88320042836bb86172eecdf85
philippguggi <58741046+philippguggi@users.noreply.github.com>
2020-04-20 08:59:49 +0200

    Constructor, ID Method, Enroll Method

--- StudentDataBase/src/com/PhilippGuggi/StudentDataBase/Student.java
+++ StudentDataBase/src/com/PhilippGuggi/StudentDataBase/Student.java
@@ -1,5 +1,66 @@
 package com.PhilippGuggi.StudentDataBase;
 
-public class Student {
+import java.util.Scanner;
 
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance = 0;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+	
+	// Constructor: prompts user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student grade level: ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
+		
+	}
+	
+	// Generate an ID
+	private void setStudentID() {
+		// Grade Level + ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
+	
+	// Enroll in courses
+	public void enroll() {
+		// Get inside a loop, user hits Q
+		do {
+			System.out.print("Enter course to enroll (Q to quit): ");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q"))
+			{
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+			} else {
+				System.out.println("Break");
+				break;
+			}
+		} while (1 != 0);
+			
+		System.out.println("Enrolled in: " + courses);
+		System.out.println();
+		System.out.println("Tuition Balance: " + tuitionBalance);
+	}
+	
+	// View balance and pay tuition
+	
+	// Show status
 }
--- StudentDataBase/src/com/PhilippGuggi/StudentDataBase/StudentDataBaseApp.java
+++ StudentDataBase/src/com/PhilippGuggi/StudentDataBase/StudentDataBaseApp.java
@@ -2,6 +2,10 @@ package com.PhilippGuggi.StudentDataBase;
 
 public class StudentDataBaseApp {
 	public static void main(String[] args) {
+		Student stud1 = new Student();
+		stud1.enroll();
+		// Ask how many users we want to add
 		
+		// Create n number of new students
 	}
 }
